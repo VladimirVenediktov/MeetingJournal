55f65033ca869a0904139af019c303e5c43b63f4
Ivan Moshna <ittifock@gmail.com>
2020-10-01 11:29:19 +0700

    git init

---
+++ src/main/java/com/meeting/app/WebMeetingAppApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMeetingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMeetingAppApplication.class, args);
+	}
+
+}
